06af37ed4ffed1a1c8c13a57c6618e7e10912b43
Nicholas Forcone <nforcone@derecho3.hsn.de.hpc.ucar.edu>
2025-05-07 08:52:08 -0600

    global unstructured grid plot

---
+++ Mesh/save-grid-visual.py
@@ -0,0 +1,5 @@
+#!/glade/work/nforcone/conda-envs/High-Resolution/bin/python
+
+from plot_unstructured import global_plot
+
+global_plot('~/CESM-Meeting/Mesh/derecho-EXODUS.g', '~/CESM-Meeting/Mesh/derecho-grid-visual')
